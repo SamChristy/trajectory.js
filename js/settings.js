5d89940716437aa732831d3fd77ec5288582cbf9
Sam Christy <sam_christy@hotmail.co.uk>
2012-04-10 22:23:01 +0200

    Update js/settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
 /**
- * @author 		Sam Christy <sam_christy@hotmail.co.uk>
+ * @author 	Sam Christy <sam_christy@hotmail.co.uk>
  * @licence     GNU GPL v3.0 <http://www.gnu.org/licenses/gpl-3.0.html>
  * @copyright   Sam Christy 2012 | All rights reserved (c)
  */
