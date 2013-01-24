292cd3070d5bd9d8cdedf2c2ea5905e43cca9b19
Sam Christy <sam_christy@hotmail.co.uk>
2013-01-24 19:04:41 +0000

    Moved the demo to github.com.

---
+++ app/js/main.js
@@ -0,0 +1,151 @@
+/**
+ * @author 	Sam Christy <sam_christy@hotmail.co.uk>
+ * @licence     GNU GPL v3.0 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @copyright   Sam Christy 2012 | All rights reserved (c)
+ */
+
+// Red: With wind resitance
+// Blue: In a vacuum
+// Purple: Kinetic energy
+
+var clearGraphButton = document.getElementById("clear-graph");
+var graphContainer = document.getElementById("graph-container");
+
+clearGraphButton.addEventListener('click', function() {
+    graphContainer.innerHTML = "";
+}, false);
+
+var genImageButton = document.getElementById("generate-image");
+genImageButton.addEventListener('click', generatePNG, false);
+
+function generatePNG() {
+	if(typeof graph === "undefined"){
+		alert("You have to plot the trajectory first!");
+		return;
+	}
+	
+	var png = graph.toImg();
+	var canvas = graph.getCanvas();
+	
+	var w = window.open("", "png", 
+		"width=" + Math.round(canvas.width * 1.04)
+		+ ",height=" + Math.round(canvas.height * 1.04)
+		+ ",modal=yes"
+	);
+
+	w.document.body.appendChild(png);
+	w.document.title = "Generated PNG";
+}
+
+var plotButton = document.getElementById("plot-trajectory");
+
+plotButton.addEventListener('click', plotTrajectory, false);
+
+/**
+ * Computes the trajectory and plots it on a graph.
+ */
+function plotTrajectory() {
+    var input = getInput();
+	
+	// Calculate the drag constant that will be used to compute the arrow's trajectory.
+	var k = dragConstant(input.mass, input.diameter, input.dragCoef, input.density);
+	
+	// Compute the trajectory using the input parameters.
+	var trajectoryInVacuum = vacTrajectory(input.velocity, input.angle, input.height);
+	
+	// Calculate the real trajectory's duration, distance and max height.
+	var bounds = trajectoryBounds(input.velocity, input.angle, input.height, 0, k);
+	
+	// Compute the real trajectory, using the precomputed duration.
+	var trajectory = arrowTrajectory(input.velocity, input.angle, input.height, k, bounds.duration);
+	
+	var settings = {
+		xGridlineCount: 10,
+		yGridlineCount: 10,
+		
+		xAxis1: {
+			min: 0,
+			max: bounds.distance,
+			title: "Distance (metres)"
+		},
+		
+		yAxis1: {
+			min: 0,
+			max: bounds.height,
+			title: "Height (metres)"
+		},
+		
+		xAxis2: null,
+		
+		yAxis2: {
+			min: 0,
+			max: 100,
+			suffix: "%",
+			title: "Kinetic Energy",
+
+			auto: false
+		}
+	};
+	
+	// Remove the graph, if it already exists.
+	graphContainer.innerHTML = "";
+	
+	graph = new Graph(835, 500, settings);
+	graph.draw();
+	graph.appendTo(graphContainer);
+	
+	// Replace each velocity value with its kinetic energy, then convert it to a percentage of the
+	// initial kinetic energy.
+	var maxK = 0.0005 * input.mass * input.velocity * input.velocity;
+	
+	for(var i = 0; i < trajectory.length; i++){
+		var v = trajectory[i][2];
+		
+		trajectory[i][2] = (0.0005 * input.mass * v * v) * 100 / maxK;
+	}
+
+	// Plot the kinetic energy.
+	graph.plotData(trajectory, Graph.colours.purple, 1, 2, 0, 2);
+	
+	// Plot the trajectories on the graph.
+	graph.plotData(trajectoryInVacuum, Graph.colours.red);
+	graph.plotData(trajectory, Graph.colours.blue);
+    
+    updateFlightStats(bounds.duration, bounds.distance, bounds.height, maxK);
+}
+
+/**
+ * Get the projectile's input data.
+ */
+function getInput() {
+    var input = {
+        "velocity" : parseFloat(document.getElementById("velocity").value),
+        "angle"    : deg2Rad(parseFloat(document.getElementById("angle").value)),
+        "height"   : parseFloat(document.getElementById("height").value),
+        "mass"     : parseFloat(document.getElementById("mass").value),
+        "diameter" : parseFloat(document.getElementById("diameter").value),
+        "dragCoef" : parseFloat(document.getElementById("drag-coefficient").value),
+        "density"  : parseFloat(document.getElementById("fluid-density").value)
+    }
+    
+    // gravity is defined globally, as it is also used by `trajectory.js`.
+    gravity = parseFloat(document.getElementById("gravity").value);
+    
+    return input;
+}
+
+function updateFlightStats(duration, distance, height, energy, precision) {
+    if (typeof precision === "undefined")
+        precision = 2;
+    
+    document.getElementById("flight-duration").textContent = duration.toFixed(precision);
+    document.getElementById("flight-distance").textContent = distance.toFixed(precision);
+    document.getElementById("flight-height").textContent   = height.toFixed(precision);
+    document.getElementById("kinetic-energy").textContent  = energy.toFixed(precision);
+};
+
+/**
+ * Validates all of the fields, to ensure that they contain sensible values (all fields are 
+ * required).
+ */
+function validateFields() {}
---
+++ app/js/trajectory.js
@@ -0,0 +1,242 @@
+/**
+ * @author 	Sam Christy <sam_christy@hotmail.co.uk>
+ * @licence     GNU GPL v3.0 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @copyright   Sam Christy 2012 | All rights reserved (c)
+ */
+
+/**
+ * Approximate gravitational acceleration.
+ */
+var gravity = 9.81;
+
+/**
+ * Calculates the drag constant for an arrow that is used to compute its trajectory with
+ * arrowTrajectory().
+ * 
+ * @param {float} mass      The arrow's mass (g).
+ * @param {float} diameter  The diameter of the arrow's shaft (mm).
+ * @param {float} dragCoef  The arrow's drag coefficient.
+ * @param {float} [density] The density of the medium, e.g. air, (kg/m³).
+ * @return {float}
+ */
+function dragConstant(mass, diameter, dragCoef, density){
+	if(!density) density =  1.29;
+	return Math.PI / 8 * 0.001 * density * (diameter * diameter) * dragCoef / mass;
+}
+
+/**
+ * Calculates the maximum height reached during the trajectory of an arrow, as well as its final
+ * distance and duration. This function is used to get an even distribution of plots across a 
+ * trajectory when using arrowTrajectory.
+ * 
+ * @param {float} velocity
+ * @param {float} angle
+ * @param {float} [height]
+ * @param {float} [bottom]
+ * @param {float} [k]
+ * @param {float} [dt]
+ * @return {object}
+ */
+function trajectoryBounds(velocity, angle, height, bottom, k, dt){
+	// Default arguments...
+	if(typeof height === "undefined") height = 0;
+	if(typeof bottom === "undefined") bottom = 0;
+	if(!k) k = 0.0037;
+	if(!dt) dt = 0.001;
+	
+	var bounds = {
+		distance: null,
+		height: null,
+		duration: null
+	};
+
+	var vx = velocity * Math.cos(angle);   // Initial horizontal velocity.
+	var vy = velocity * Math.sin(angle);   // Initial vertical velocity.
+	var distance = 0;                      // Distance travelled by the arrow.
+	var dtdt = dt * dt;                    // The time interval squared.
+	
+	var dx;	  // The horizontal distance covered each time interval, dt.
+	var dy;	  // The vertical distance covered each time interval, dt.
+	var a;	  // Vector deceleration.
+	var ax;   // Horizontal deceleration due to air resistance.
+	var ay;	  // Vertical deceleration due to air resistance and gravity.
+	
+	// We stop when the arrow lands, i.e. its height < bottom.
+	for(var t = 0; height >= bottom; t += dt){
+		// Compute drag from initial velocity.
+		a = -k * velocity * velocity;
+		
+		// Compute the X and Y vector components of drag and velocity.
+		ax = a * Math.cos(angle);
+		ay = a * Math.sin(angle);
+		
+		// Compute the change in X and Y position.
+		dx = vx * dt + 0.5 * ax * dtdt; 
+		dy = vy * dt + 0.5 * (ay - gravity) * dtdt;
+		
+		// Move the arrow.
+		distance += dx;
+		height += dy;
+		
+		// Compute new vector velocity.
+		velocity = Math.sqrt(Math.pow(dx / dt, 2) + Math.pow(dy / dt, 2));
+		
+		// Compute new X and Y velocities.
+		vx += ax * dt;
+		vy += (ay - gravity) * dt;
+		
+		// Compute new arrow flight angle.
+		angle = Math.atan2(dy, dx);
+		
+		if(dy <= 0 && bounds.height === null)  // We've reached our max height, let's record it.
+			bounds.height = height;
+	}
+	
+	// Record the final distance and duration.
+	bounds.distance = distance;
+	bounds.duration = t;
+	
+	return bounds;
+}
+
+/**
+ * Computes the trajectory of an arrow with air resistance.
+ * 
+ * @param {float} velocity    The arrow's initial horizontal velocity (metres / second).
+ * @param {float} angle	      The arrow's initial angle (radians).
+ * @param {float} [height]    The arrow's initial height (metres).
+ * @param {float} [k]         The drag equation constant for the arrow.
+ * @param {int}   [dt]        The time interval for calculations (seconds).
+ * @param {int}   [plotCount]
+ * @return {array}
+ */
+function arrowTrajectory(velocity, angle, height, k, duration, dt, plotCount){
+	// Default arguments...
+	if(typeof height === "undefined") height = 0;
+	if(!k) k = 0.0037;
+	if(!duration) duration = 2;
+	if(!dt) dt = 0.001;
+	if(!plotCount) plotCount = 100;
+	
+	var data = new Array(plotCount);       // We'll store the plots here.
+	var vx = velocity * Math.cos(angle);   // Initial horizontal velocity.
+	var vy = velocity * Math.sin(angle);   // Initial vertical velocity.
+	var distance = 0;                      // Distance travelled by the arrow.
+	var dtdt = dt * dt;                    // The time interval squared.
+	
+	var dx;	  // The horizontal distance covered each time interval, dt.
+	var dy;	  // The vertical distance covered each time interval, dt.
+	var a;	  // Vector deceleration.
+	var ax;   // Horizontal deceleration due to air resistance.
+	var ay;	  // Vertical deceleration due to air resistance and gravity.
+	
+	
+	data[0] = [distance, height, velocity];
+	
+	for(var t = 0, plot = 1; t <= duration, plot < plotCount; t += dt){
+		// Compute drag from initial velocity.
+		a = -k * velocity * velocity;
+		
+		// Compute the X and Y vector components of drag and velocity.
+		ax = a * Math.cos(angle);
+		ay = a * Math.sin(angle);
+		
+		// Compute the change in X and Y position.
+		dx = vx * dt + 0.5 * ax * dtdt; 
+		dy = vy * dt + 0.5 * (ay - gravity) * dtdt;
+		
+		// Move the arrow.
+		distance += dx;
+		height += dy;
+		
+		// Compute new vector velocity.
+		velocity = Math.sqrt(Math.pow(dx / dt, 2) + Math.pow(dy / dt, 2));
+		
+		// Compute new X and Y velocities.
+		vx += ax * dt;
+		vy += (ay - gravity) * dt;
+		
+		// Compute new arrow flight angle.
+		angle = Math.atan2(dy, dx);
+		
+		if(Math.floor(t / duration * plotCount) > plot){
+			data[plot] = [distance, height, velocity];
+			plot++;
+		}
+	}
+    
+	return data;
+}
+
+/**
+ * Calculates the distance reached by a projectile in a vacuum.
+ * 
+ * @param {Float} angle The launch angle in radians.
+ * @param {Float} velocity The velocity of the projectile.
+ * @param {Float} height The initial height of the projectile (archer's chin  for an arrow).
+ * @return {Float}
+ */
+function vacFlightDistance(angle, velocity, height){
+	var vSinAngle = Math.sin(angle) * velocity;
+	
+	return (velocity * Math.cos(angle) / g) * (vSinAngle + Math.sqrt(vSinAngle * vSinAngle + 2 * gravity * height));
+}
+
+/**
+ * Calculates the maximum height reached by a projectile, in a vacuum.
+ * 
+ * @param {Float} angle    The initial angle (radians).
+ * @param {Float} velocity 
+ * @param {Float} height   The projectile's initial height.
+ * @return {Float}
+ */
+function vacFlightHeight(angle, velocity, height){
+	var sinAngle = Math.sin(angle);
+	
+	return (velocity * velocity * sinAngle * sinAngle) / (2 * gravity) + height;
+}
+
+/**
+ * Calculates the flight duration of a proctile, in a vacuum.
+ * 
+ * @param {Float} angle The initial angle in radians.
+ * @param {Float} velocity
+ * @param {Float} height The projectile's initial height.
+ * @return {Float}
+ */
+function vacFlightDuration(angle, velocity, height){
+	var vSinAngle = Math.sin(angle) * velocity;
+	
+	return (vSinAngle + Math.sqrt(vSinAngle * vSinAngle + 2 * gravity * height)) / gravity;
+}
+
+/**
+ * Calculates the (parabolic) trajectory of a projectile in a vacuum.
+ * 
+ * @param {float} velocity    The projectile's initial velocity (metres/second).
+ * @param {float} angle       The projectile's initial angle (radians).
+ * @param {float} [height]    The projectile's initial height (metres).
+ * @param {float} [bottom]    The value on the y axis at which to stop the trajectory.
+ * @param {int}   [plotCount] The number of plots in the trajectory. 
+ */
+function vacTrajectory(velocity, angle, height, bottom, plotCount){
+	if(typeof height === "undefined") height = 0;
+	if(typeof bottom === "undefined") bottom = 0;
+	if(typeof plotCount === "undefined") plotCount = 100;
+	
+	var data = new Array(plotCount);
+	var duration = vacFlightDuration(angle, velocity, height - bottom);
+	
+	plotCount--;
+	
+	for(var plot = 0; plot <= plotCount; plot++){
+		var time = plot / plotCount * duration;
+		
+		data[plot] = [
+			velocity * time * Math.cos(angle),
+			velocity * time * Math.sin(angle) - gravity / 2 * (time * time) + height
+		];
+	}
+	
+	return data;
+}
